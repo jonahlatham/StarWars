f25b7db6c601e834899fe7daa2cbe972beb5f797
Jonah Latham <jonahlatham@jonahs-air.lan>
2019-06-16 18:48:40 -0600

    Star Wars

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const baseUrl = 'https://swapi.co/api/people/'
+
+class App extends Component {
+  state = {
+    starWarsLoop: [],
+  }
+  componentDidMount() {
+    const starWarsData = [];
+    for (let i = 1; i <= 5; i++) {
+      starWarsData.push(axios.get(`${baseUrl}/${i}`))
+    }
+    Promise.all(starWarsData)
+      .then((response) => {
+        this.setState({
+          starWarsLoop: response.map((e, i) => {
+            console.log(e.data)
+            return e.data
+          })
+        })
+      })
+  }
+  render() {
+    const starWarsGuys = this.state.starWarsLoop.map((e, i) => {
+      return <div key={i}>{e.name}</div>
+    })
+    return (
+      <div className="App">
+        {starWarsGuys}
+      </div>
+    );
+  }
 }
-
 export default App;
+
+
+// boba fett hit list 
+// star wars characters
+// make a hit list for boba fett
+// button called kill
+// when kill it moves car from left to right and
+        // changes it 
